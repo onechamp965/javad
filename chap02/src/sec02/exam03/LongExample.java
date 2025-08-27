3a09b5daff16b257d9a6d934ec5bd380441ca4f9
onechamp965 <onechamp965@gmail.com>
2025-08-27 10:17:17 +0900

    First Commit

---
+++ chap02/src/sec02/exam03/LongExample.java
@@ -0,0 +1,17 @@
+package sec02.exam03;
+
+public class LongExample {
+
+	public static void main(String[] args) {
+		long var1 = 10;
+		long var2 = 20L;
+		long var3 = 1000000000; // 0 10개를 정수 범위를 초과하면 L사용 필
+		long var4 = 1000000000000L;
+
+		System.out.println(var1);
+		System.out.println(var2);
+		System.out.println(var3);
+		System.out.println(var4);
+	}
+
+}
