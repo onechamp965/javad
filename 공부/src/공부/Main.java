e9fad2986f7bb9d8cfa4478490bd10742620a611
onechamp965 <onechamp965@gmail.com>
2025-09-08 19:32:32 +0900

    java

---
+++ 공부/src/공부/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		System.out.println(n + "*" + 1 + "=" + n * 1);
+		System.out.println(n + "*" + 2 + "=" + n * 2);
+		System.out.println(n + "*" + 3 + "=" + n * 3);
+		System.out.println(n + "*" + 4 + "=" + n * 4);
+		System.out.println(n + "*" + 5 + "=" + n * 5);
+		System.out.println(n + "*" + 6 + "=" + n * 6);
+		System.out.println(n + "*" + 7 + "=" + n * 7);
+		System.out.println(n + "*" + 8 + "=" + n * 8);
+		System.out.println(n + "*" + 9 + "=" + n * 9);
+	}
+}
